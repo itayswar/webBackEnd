fc1248d7be9aef0457942bcf6ad6d3ef1a00fdf8
Itay Swartzstein <itayswar@post.bgu.ac.il>
2022-01-14 09:32:20 +0200

    Ex 10

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
 from flask import Flask, redirect, url_for, request
-from flask import render_template , session, blueprints
+from flask import render_template, session, blueprints
 import mysql.connector
+import json
+import requests
+
 
 app = Flask(__name__)
 users  = ['Itay', 'Yossi', 'Dana', 'Mor', 'Yuval']
 app.secret_key = '123'
+
+
+def getUsers(index):
+    ret = requests.get(f'https://reqres.in/api/users/{index}')
+    return ret.json()
+
+
+def interact_db(query, query_type: str):
+    return_value = False
+    connection = mysql.connector.connect(host='localhost',
+                                         user='root',
+                                         passwd='itay3636',
+                                         database='ass_10')
+    cursor = connection.cursor(named_tuple=True)
+    cursor.execute(query)
+    #
+
+    if query_type == 'commit':
+        # Use for INSERT, UPDATE, DELETE statements.
+        # Returns: The number of rows affected by the query (a non-negative int).
+        connection.commit()
+        return_value = True
+
+    if query_type == 'fetch':
+        # Use for SELECT statement.
+        # Returns: False if the query failed, or the result of the query if it succeeded.
+        query_result = cursor.fetchall()
+        return_value = query_result
+
+    connection.close()
+    cursor.close()
+    return return_value
+
+
+# ------------------------------------------------- #
+# ------------------------------------------------- #
 
 # ------------------------------------------------- #
 # ------------- DATABASE CONNECTION --------------- #
@@ -50,6 +89,32 @@ def ass9():
                            messege = messege,
                            currMethod = currMethod
                            )
+
+@app.route('/assignment11')
+def ass11():
+    return render_template('assignment11.html', ind="ind", list='')
+
+
+@app.route('/assignment11/users', methods = ['POST', 'GET'])
+def ass11_users():
+    dic = {}
+    for user in interact_db(query="SELECT * FROM ass_10.users;", query_type='fetch'):
+        dic[f'user_{user.name}'] = {
+            'name': user.name
+        }
+    return render_template('assignment11.html', list=dic, ind="ind")
+
+@app.route('/assignment11/outer_source',  methods=['post'])
+def outerSource():
+    if "front" in request.form:
+        num = int(request.form['front'])
+        return render_template('assignment11.html', front=num)
+    elif "back" in request.form:
+        num = int(request.form["back"])
+        user = getUsers(num)
+        return render_template('assignment11.html', back=user)
+    else:
+        return render_template('assignment11.html')
 
 
 if __name__ == '__main__':
